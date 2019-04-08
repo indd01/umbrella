19fc07349b9ee3ab92fa627cd8d61544f11b98ed
Sergey Nezbritskiy <sergeyn@corra.com>
2019-04-08 18:34:50 +0300

    update installation script, improvements for reset example

--- bin/install.sh
+++ bin/install.sh
@@ -24,6 +24,16 @@ bash ../bin/_project.sh
 
 cd ../8-reset-simple-example
 bash ../bin/_project.sh
+echo "Завтра забудешь, что тебя короновал,
+что душу цветущую любовью выжег,
+несуетных дней взметенный карнавал
+растреплет страницы моих книжек...
+" >> mayakovskiy.txt
+git add mayakovskiy.txt
+echo "Слов моих сухие листья ли
+заставят остановиться, жадно дыша?
+Дай хоть последней нежностью выстелить
+твой уходящий шаг." >> mayakovskiy.txt
 
 cd ../9-rerere-simple-example
 bash ../bin/_project.sh
